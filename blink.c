c7db5af96df4cb61f070628475312a69e6840e14
four0four <shmeeeh@gmail.com>
2014-03-25 21:28:10 -0400

    changed driverlib calls to ROM_

--- blink.c
+++ blink.c
@@ -4,13 +4,14 @@
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/rom.h"
 
 void init(void)
 {
-	SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_3);
-	GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0xFF);
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_3);
+	ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0xFF);
 }
 
 int main(void)
@@ -18,9 +19,9 @@ int main(void)
 	init();
 	while(1)
 	{
-		SysCtlDelay(2666666);
-		GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0x00);
-		SysCtlDelay(2666666);
-		GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0xFF);
+		ROM_SysCtlDelay(2666666);
+		ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0x00);
+		ROM_SysCtlDelay(2666666);
+		ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0xFF);
 	}
 }
